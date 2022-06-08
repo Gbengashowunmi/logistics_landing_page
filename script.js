7554a8eb1ad1bbda60361425019ced86cfc7e321
Gbengashowunmi <96231197+Gbengashowunmi@users.noreply.github.com>
2022-06-08 01:37:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ const lineB = document.getElementById("line2");
 const lineC = document.getElementById("line3");
 const feat = document.querySelector(".feature");
 const ul = document.querySelector(".navigation");
+const body = document.querySelector(".body");
 
 const company = document.querySelector(".company");
 const companyContent = document.querySelector(".company-content");
 const featContent = document.querySelector(".feat-content");
 const overlay = document.querySelector(".overlay");
+const feature = document.querySelector(".feature");
 
 navBar.addEventListener("click", ()=>{
     // console.log('clicked');
@@ -17,6 +19,10 @@ navBar.addEventListener("click", ()=>{
     lineC.classList.toggle("active");
     ul.classList.toggle("active");
     overlay.classList.toggle('active');
+    
+})
+body.addEventListener("click", ()=>{
+    console.log('clicked'); 
 })
 let featActive;
 let companyActive;
@@ -24,10 +30,12 @@ let companyActive;
 feat.addEventListener('click', ()=>{
     
     featureActive =  featContent.classList.toggle('active');
+    feature.classList.toggle('active');
     
 })
 company.addEventListener('click', ()=>{
-    companyActive = companyContent.classList.toggle('active')
+    companyActive = companyContent.classList.toggle('active');
+    company.classList.toggle('active');
 })
 
 // cross.addEventListener("click", ()=>{
